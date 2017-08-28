2232cf65874879f64fa74134677109b46d4b86e6
Nemanja <nemanja.jakovljevic17@gmail.com>
2017-08-28 23:27:29 +0200

    30.8.2017 16:17

--- app/src/main/java/android/parkskocjanskejame/Tabla16.java
+++ app/src/main/java/android/parkskocjanskejame/Tabla16.java
@@ -1,13 +1,98 @@
 package android.parkskocjanskejame;
 
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Handler;
+import android.parkskocjanskejame.utils.RateView;
+import android.parkskocjanskejame.utils.RateViewListener;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
-public class Tabla16 extends AppCompatActivity {
+import static android.parkskocjanskejame.R.string.gumbZnova;
+import static android.parkskocjanskejame.R.string.naprej;
+
+
+public class Tabla16 extends AppCompatActivity implements RateViewListener {
+
+    //Client side variables
+    RateView rateView;
+    AlertDialog.Builder alertDialog;
+    AlertDialog alert;
+    LayoutInflater inflater;
+    View view;
+    Typeface font;
+    ScrollView sv;
+    String odgovor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tabla16);
+
+       /* //Setup the number font
+        font = Typeface.createFromAsset(getAssets(), "dinFont.ttf");
+        confidence = (TextView)findViewById(R.id.confidence);
+        confidence.setTypeface(font);*/
+
+        //Setup event listener for drawing
+        rateView = (RateView) findViewById(R.id.rateView);
+        rateView.setCustomEventListener(this);
     }
+
+    @Override
+    public void onMove(int movedX, int maxHeight) {
+
+    }
+
+    @Override
+    public void onSwipe(boolean prav) {
+        AlertDialog.Builder aD = new AlertDialog.Builder(Tabla16.this);
+        LayoutInflater inf = getLayoutInflater();
+        View v = inf.inflate(R.layout.tabla3apopup, null);
+        aD.setView(v);
+        alert = aD.create();
+        alert.setCancelable(false);
+        alert.show();
+
+        TextView text = (TextView) v.findViewById(R.id.textView23);
+        text.setText(R.string.tabla19razlaga);
+        final ImageView tabla = (ImageView) v.findViewById(R.id.imageTabla3a);
+        tabla.setImageResource(R.drawable.tabla16);
+
+        if (prav) {
+            //Continue button
+            Button continueButton = (Button) v.findViewById(R.id.button2345);
+            continueButton.setText(naprej);
+            continueButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(getApplicationContext(), Cestitamo.class);
+                    startActivity(intent);
+                }
+            });
+
+        } else {
+            //Retry button
+            Button continueButton = (Button) v.findViewById(R.id.button2345);
+            continueButton.setText(gumbZnova);
+            continueButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    alert.cancel();
+                }
+            });
+
+        }
+
+    }
+
 }
+
+
